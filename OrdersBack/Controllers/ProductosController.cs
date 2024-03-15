eb00547ed5499c8836e8fc272d34097a9d5b3344
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-15 11:50:01 -0500

    feat: add productos controller

---
+++ OrdersBack/Controllers/ProductosController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrdersBack.DTOs;
+using OrdersBack.Models;
+
+namespace OrdersBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly BdatosContext _context;
+        public ProductosController(BdatosContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/producto/{key}
+        [HttpGet("{key}")]
+        public async Task<ActionResult<IEnumerable<ProductoDTO>>> GetProducto(string key)
+        {
+            var productoQuery = _context.Artis.Where(a => a.ArtCodcia == "01" && a.ArtSituacion == "0");
+
+            if (int.TryParse(key, out int codigo))
+            {
+                productoQuery = productoQuery.Where(a => a.ArtKey == codigo);
+            }
+            else
+            {
+                productoQuery = productoQuery.Where(a => a.ArtNombre.Contains(key));
+            }
+
+            var productos = await productoQuery.ToListAsync();
+            if (productos == null || !productos.Any())
+            {
+                return NotFound("No se encontraron productos.");
+            }
+
+            var productosDtos = new List<ProductoDTO>();
+
+            foreach (var producto in productos)
+            {
+                var articulo = await _context.Articulos.FirstOrDefaultAsync(a => a.ArmCodart == producto.ArtKey && a.ArmCodcia == "01");
+                var precio = await _context.Precios.FirstOrDefaultAsync(a => a.PreCodart == producto.ArtKey && a.PreCodcia == "01");
+                var marca = await _context.Tablas.FirstOrDefaultAsync(a => a.TabNumtab == producto.ArtFamilia && a.TabTipreg == 122 && a.TabCodcia == "01");
+
+                if (articulo != null)
+                {
+                    if (articulo.Comprometido == null)
+                    {
+                        articulo.Comprometido = 0;
+                    }
+                    productosDtos.Add(new ProductoDTO
+                    {
+                        Codigo = (int)producto.ArtKey,
+                        Descripcion = producto.ArtNombre.Trim(),
+                        Marca = marca.TabNomlargo.Trim(),
+                        Presentacion = precio.PreUnidad.Trim(),
+                        Precio = (decimal)precio.PrePre1,
+                        Stock = (int)articulo.ArmStock - (int)articulo.Comprometido
+                    });
+                }
+                else
+                {
+                    return NotFound("ERROR: Consulte al administrador de base de datos.");
+                }
+            }
+            return productosDtos;
+        }
+    }
+}

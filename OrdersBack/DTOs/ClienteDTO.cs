930bc546cf99593dd71bd2797bc342a308502473
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-18 08:57:21 -0500

    feat: add cliente dto

---
+++ OrdersBack/DTOs/ClienteDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace OrdersBack.DTOs
+{
+    public class ClienteDTO
+    {
+        public int Codigo { get; set; }
+        public string? Documento { get; set; }
+        public string? Razon { get; set; }
+        public string? Nombre { get; set; }
+        public string? Comercial { get; set; }
+        public List<DireccionDTO> Direcciones { get; set; }
+        public int Vendedor { get; set; }
+    }
+
+    public class DireccionDTO
+    {
+        public int Codigo { get; set; }
+        public string? Direccion { get; set; }
+        public string? Distrito { get; set; }
+        public string? Provincia { get; set; }
+        public string? Departamento { get; set; }
+    }
+}

a10496e2bffdbf23e07e8732ad51588001365d1a
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-22 10:02:39 -0500

    feat: add vendedor dto

---
+++ OrdersBack/DTOs/VendedorDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace OrdersBack.DTOs
+{
+    public class VendedorDTO
+    {
+        public int Codigo { get; set; }
+        public string? Nombre { get; set; }
+        public string? Clave { get; set; }
+    }
+}

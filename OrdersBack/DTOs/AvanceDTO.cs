7ce9d704e86317ad541159ecd277f6075045fbba
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-05-29 09:21:32 -0500

    feat: add avance dto

---
+++ OrdersBack/DTOs/AvanceDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace OrdersBack.DTOs
+{
+    public class AvanceDTO
+    {
+        public int Vendedor { get; set; }
+        public decimal Avance { get; set; }
+    }
+
+    public class CuotaDto
+    {
+        public int Vendedor { get; set; }
+        public decimal Cuota { get; set; }
+    }
+}
